8c2c88b2a2a61bf2fe30722f240b8cd3f7131891
Yukihiro Terakado <yukihiro.terakado@gmail.com>
2025-07-25 10:59:16 +0900

    fix script file

--- download_organizer.sh
+++ download_organizer.sh
@@ -1,26 +1,20 @@
 #!/bin/bash
 
-# ダウンロードフォルダのパス
 DOWNLOAD_DIR="$HOME/Downloads"
 
-# 現在の年と月を取得
 YEAR_MONTH=$(date +"%Y_%m")
 
-# 移動先のフォルダを作成（存在しなければ）
 DEST_DIR="$DOWNLOAD_DIR/$YEAR_MONTH"
 mkdir -p "$DEST_DIR"
 
-# ダウンロードフォルダ内の全ファイルを処理
 find "$DOWNLOAD_DIR" -maxdepth 1 -type f | while read FILE; do
   BASENAME=$(basename "$FILE")
   DEST_FILE="$DEST_DIR/$BASENAME"
   
-  # 同名ファイルが存在する場合、ファイル名を変更
   if [ -e "$DEST_FILE" ]; then
     COUNTER=1
     EXT="${BASENAME##*.}"
     NAME="${BASENAME%.*}"
-    # 拡張子なしファイル対応
     if [ "$NAME" = "$BASENAME" ]; then
       EXT=""
     else
